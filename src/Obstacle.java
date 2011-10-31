84dc2da9f971b9015047abba2c3de4f97bca4a82
Julian Møller <julian.moeller@gmail.com>
2011-10-31 12:33:03 +0100

    Update settings and clean up files using them

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,27 +1,34 @@
-
 import processing.core.*;
-public class Obstacle {
-	
+
+public class Obstacle
+{
+
 	PApplet parent;
 	public int height;
 	public int width;
 	public float xPos;
 	public float yPos;
-	public Obstacle(PApplet p, float x, float y, int w, int h){
-		
+
+	public Obstacle(PApplet p, float x, float y, int w, int h)
+	{
+
 		parent = p;
 		height = h;
 		width = w;
 		xPos = x;
 		yPos = y;
-		
+
 	}
-	public void draw(){
+
+	public void draw()
+	{
 		parent.fill(255);
 		parent.stroke(255);
 		parent.rect(xPos, yPos, width, height);
 	}
-	public boolean collidesWith(Player p){
-		return (p.y >= yPos && p.y <= (yPos+height) && p.x >= xPos && p.x <= (xPos+width));
+
+	public boolean collidesWith(Player p)
+	{
+		return (p.y >= yPos && p.y <= (yPos + height) && p.x >= xPos && p.x <= (xPos + width));
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,8 @@
 import processing.core.*;
 
+public class Player
+{
 
-public class Player {
-	
 	public float x;
 	public float y;
 	public PImage texture;
@@ -16,11 +16,12 @@ public class Player {
 	private static float BRAKESPEED = 3f;
 	private float lastGroundedY;
 	private float currentXSpeed;
-	
+
 	public static float GRAVITY = 9.82f;
-	public static float AIRFACTOR = 5.0f/10.0f;
-	
-	public Player(BunnyHat p){
+	public static float AIRFACTOR = 5.0f / 10.0f;
+
+	public Player(BunnyHat p)
+	{
 		parent = p;
 		x = 200;
 		y = 200;
@@ -32,66 +33,75 @@ public class Player {
 		lastGroundedY = 0;
 		texture = parent.loadImage("../player.png");
 	}
-	
-	public void update(){
+
+	public void update()
+	{
 		boolean hasCollided = false;
 		x += currentXSpeed;
 		float sign = Math.signum(currentXSpeed);
-		currentXSpeed = currentXSpeed-BRAKESPEED*sign;
-		if (Math.abs(sign - Math.signum(currentXSpeed)) > 1f){
+		currentXSpeed = currentXSpeed - BRAKESPEED * sign;
+		if (Math.abs(sign - Math.signum(currentXSpeed)) > 1f)
+		{
 			currentXSpeed = 0;
 		}
-		for(Obstacle o : parent.obstacleList){
-			if (o.collidesWith(this)){
+		for (Obstacle o : parent.obstacleList)
+		{
+			if (o.collidesWith(this))
+			{
 				hasCollided = true;
 				break;
 			}
 		}
-		
-		
-		
-		if (isJumping && y < (lastGroundedY-JUMPHEIGHT)){
+
+		if (isJumping && y < (lastGroundedY - JUMPHEIGHT))
+		{
 			isJumping = false;
 		}
 		this.isInAir = !hasCollided;
-		
-		if (isJumping) {
+
+		if (isJumping)
+		{
 			ySpeed = JUMPSPEED;
 		}
-		else if (this.isInAir){
+		else if (this.isInAir)
+		{
 			ySpeed = GRAVITY;
 		}
-		else{
+		else
+		{
 			ySpeed = 0;
 		}
-		y+=ySpeed;
+		y += ySpeed;
 	}
-	
-	void jump(){
-		if(!this.isInAir){
+
+	void jump()
+	{
+		if (!this.isInAir)
+		{
 			lastGroundedY = y;
 			isJumping = true;
 		}
 	}
-	
-	void draw(){
-		float drawX = x-texture.width/2;
-		float drawY = y-texture.height;
+
+	void draw()
+	{
+		float drawX = x - texture.width / 2;
+		float drawY = y - texture.height;
 		parent.stroke(0);
-		parent.fill(parent.color(255,0,0,255));
+		parent.fill(parent.color(255, 0, 0, 255));
 		parent.ellipse(x, y, 4, 4);
 		parent.image(texture, drawX, drawY);
 
 	}
-	
-	public void moveRight(){
+
+	public void moveRight()
+	{
 		currentXSpeed = xSpeed;
 	}
 
-	public void moveLeft(){
+	public void moveLeft()
+	{
 		currentXSpeed = -xSpeed;
 	}
-	
-	
-	
+
 }
