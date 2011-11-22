be4991989b41a2a5f8ad8f3a2ce09e25890a0773
Samuel Walz <samuel.walz@gmail.com>
2011-11-22 22:56:54 +0100

    Introducing two abstract class: Creature & CrossingCreature
    Creature: exists only in one dream temporarily
    CrossingCreature: existing in both dreams

---
+++ src/game/creatures/Creature.java
@@ -0,0 +1,14 @@
+package game.creatures;
+
+/**
+ * Standard creature - only existing in one of the dreams
+ * 
+ * use it for e.g. temporary creatures in just one dream
+ * 
+ * @author samuelwalz
+ *
+ */
+public abstract class Creature
+{
+
+}
---
+++ src/game/creatures/CrossingCreature.java
@@ -0,0 +1,15 @@
+package game.creatures;
+
+/**
+ * Crossing Creature
+ * this kind of creature has a representation in both dreams
+ * 
+ * this one is to use in most cases
+ * 
+ * @author samuelwalz
+ *
+ */
+public abstract class CrossingCreature extends Creature
+{
+
+}
